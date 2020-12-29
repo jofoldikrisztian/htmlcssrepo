ee36a77200246ade3a1f0b7f263c686fbe54be9f
jofoldikrisztian <krisztianjofoldi@gmail.com>
2020-12-30 00:45:39 +0100

    Sticky Navbar

--- 05. Responsive Navigation Bar (Mobile menu)/main.js
+++ 05. Responsive Navigation Bar (Mobile menu)/main.js
@@ -3,11 +3,11 @@ var isActive = false;
 $('.menu').on('click', function (){
     if (isActive){
         $(this).removeClass('active');
-        $('body').removeClass('menu-open');
+        $('header').removeClass('menu-open');
     }
     else {
         $(this).addClass('active');
-        $('body').addClass('menu-open');
+        $('header').addClass('menu-open');
     }
 
     isActive = !isActive;
