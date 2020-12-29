899bc6b8ac8f2de4101fca5ed017af19a71f618a
jofoldikrisztian <krisztianjofoldi@gmail.com>
2020-12-29 22:47:50 +0100

    Responsive navigation bar

---
+++ 05. Responsive Navigation Bar (Mobile menu)/main.js
@@ -0,0 +1,14 @@
+var isActive = false;
+
+$('.menu').on('click', function (){
+    if (isActive){
+        $(this).removeClass('active');
+        $('body').removeClass('menu-open');
+    }
+    else {
+        $(this).addClass('active');
+        $('body').addClass('menu-open');
+    }
+
+    isActive = !isActive;
+});
